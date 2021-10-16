79f6c9a84847183656952410eeb50883c3f0aa06
user <user@server.localnet.jp>
2021-10-16 21:13:36 +0900

    update javascript files

--- express/src/routes/todo_lists.js
+++ express/src/routes/todo_lists.js
@@ -1,5 +1,5 @@
 const {defaultFormatter, ORMapper} = require('./utils.js');
-const CustomRouter = require('./common.js');
+const CustomRouter = require('./custom_router.js');
 const models = require('./database.js').models;
 const orm = new ORMapper(models.ToDoList);
 const customRouter = new CustomRouter('ToDoList', orm, defaultFormatter(['limit']));
--- express/src/routes/users.js
+++ express/src/routes/users.js
@@ -1,5 +1,5 @@
 const {defaultFormatter, ORMapper} = require('./utils.js');
-const CustomRouter = require('./common.js');
+const CustomRouter = require('./custom_router.js');
 const models = require('./database.js').models;
 const orm = new ORMapper(models.User);
 const customRouter = new CustomRouter('User', orm, defaultFormatter(['birthday']));
